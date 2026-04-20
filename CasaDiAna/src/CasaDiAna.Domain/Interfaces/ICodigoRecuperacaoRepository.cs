303a0a69eb1414607b231b16e7b3e5dd95eea5de
Gustavo Leal <gustavoleal1194@gmail.com>
2026-04-19 22:11:42 -0300

    fix(domain): CancellationToken = default em ICodigoRecuperacaoRepository

--- CasaDiAna/src/CasaDiAna.Domain/Interfaces/ICodigoRecuperacaoRepository.cs
+++ CasaDiAna/src/CasaDiAna.Domain/Interfaces/ICodigoRecuperacaoRepository.cs
@@ -1,12 +1,11 @@
-// src/CasaDiAna.Domain/Interfaces/ICodigoRecuperacaoRepository.cs
 using CasaDiAna.Domain.Entities;
 
 namespace CasaDiAna.Domain.Interfaces;
 
 public interface ICodigoRecuperacaoRepository
 {
-    Task AdicionarAsync(IEnumerable<CodigoRecuperacao> codigos, CancellationToken ct);
-    Task<IReadOnlyList<CodigoRecuperacao>> ObterAtivosPorUsuarioAsync(Guid usuarioId, CancellationToken ct);
-    Task MarcarUsadoAsync(Guid id, CancellationToken ct);
-    Task DeletarPorUsuarioAsync(Guid usuarioId, CancellationToken ct);
+    Task AdicionarAsync(IEnumerable<CodigoRecuperacao> codigos, CancellationToken ct = default);
+    Task<IReadOnlyList<CodigoRecuperacao>> ObterAtivosPorUsuarioAsync(Guid usuarioId, CancellationToken ct = default);
+    Task MarcarUsadoAsync(Guid id, CancellationToken ct = default);
+    Task DeletarPorUsuarioAsync(Guid usuarioId, CancellationToken ct = default);
 }
